08e2e7d9e3edfa9ed8fda0facb767d458690cfc2
Max <max.alt@gmx.de>
2024-09-20 13:44:13 +0200

    rework of routes

--- src/index.ts
+++ src/index.ts
@@ -2,18 +2,20 @@ import express from 'express';
 import swaggerUi from 'swagger-ui-express';
 import swaggerJSDoc from 'swagger-jsdoc';
 import swaggerOptions from './swaggerOptions.js';
-import hygrometer from './routes/hygrometer.js';
+import hygrometerRoutes from './routes/hygrometer.js';
 
 const app = express();
 const port = 3000;
 
 app.use(express.json());
 
-app.use('/api/test', hygrometer);
+
 
 // Swagger Docs
 const swaggerDocs = swaggerJSDoc(swaggerOptions);
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
+
+app.use('/hygrometer', hygrometerRoutes);
 
 // Catch-All Route für nicht gefundene Endpunkte
 app.use((req, res) => {
--- src/routes/hygrometer.ts
+++ src/routes/hygrometer.ts
@@ -4,20 +4,55 @@ const router = express.Router();
 import pool from '../database.js';
 
 
-// In einer der Routendateien
 /**
  * @swagger
- * /api/test:
+ * tags:
+ *  name: Hygrometer
+ *  description: API endpoints for managing hygrometer sensor data
+*/
+
+/**
+ * @swagger
+ * /hygrometer:
  *   get:
- *     summary: Test route
- *     description: This is a test route
+ *     tags: [Hygrometer]
+ *     summary: Get all Hygrometers 
+ *     description: Displays latest hygrometer sensors data 
  *     responses:
  *       200:
  *         description: Successful response
+ *       400:
+ *         description: Bad Request
  */
 router.get('/', (req, res) => {
-  res.send('Test route');
+  res.send('Test route das ein test');
 });
+
+/**
+ * @swagger
+ * /hygrometer/{id}:
+ *  get:
+ *     tags: [Hygrometer]
+ *     summary: Get all Hygrometers 
+ *     description: Displays latest hygrometer sensors data 
+ *     parameters:
+ *      - in: path
+ *        name: id
+ *        required: true
+ *        schema:
+ *          type: integer
+ *        description: The hygrometer sensor ID
+ * 
+ *     responses:
+ *       200:
+ *         description: Successful response
+ *       400:
+ *         description: Bad Request
+ * 
+ */
+router.get('/:id',async(req, res) => {
+  res.send("hymudity id")
+})
 
 router.use((req, res) => {
   res.status(404).json({ message: 'Route nicht gefunden - route' });
