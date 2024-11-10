43f6313330b3eca0e175aad2c50ab6e251bb552c
Max <max.alt@gmx.de>
2024-11-10 11:35:09 +0100

    [imp] new  post function for humidity sensor. Upload should work. nyi testing

--- src/routes/hygrometer.ts
+++ src/routes/hygrometer.ts
@@ -1,7 +1,7 @@
 import express from 'express';
 const router = express.Router();
 
-import pool from '../database.js';
+import database from '../database.js';
 
 
 /**
@@ -50,9 +50,58 @@ router.get('/', (req, res) => {
  *         description: Bad Request
  * 
  */
-router.get('/:id',async(req, res) => {
-  res.send("hymudity id")
-})
+router.get('/:id', async (req, res) => {
+  try {
+    const query = 'SELECT temperature,humidity,sensornr FROM DHT11 WHERE id = ?';
+    let id = req.params.id;
+    // Ausführen der SQL-Abfrage
+    const result = await database.query(query,[id]);
+    console.log(result);
+    res.json(result);
+} catch (error) {
+}
+});
+
+/**
+ * @swagger
+ * /hygrometer:
+ *   post:
+ *     summary: Create a new Sensor reading
+ *     tags: [Hygrometer]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             required:
+ *               - value
+ *             properties:
+ *               value:
+ *                 type: number
+ *     responses:
+ *       '201':
+ *         description: Sensor reading created successfully
+ */
+router.post('/', async (req, res) => {
+  try {
+    const query ='INSERT INTO DHT11 (Temperature,Humidity,Sensornr,DATETIME) VALUES (?,?)';
+    const { Temperature, Humidity, Sensornr } = req.body;
+    
+    let datetime = new Date(); 
+    console.log(datetime);
+    const result = await database.query(query, [Temperature, Humidity, Sensornr, datetime]);
+    //res.json({ message: 'Temperature added successfully', result });
+    console.log(result);
+    res.status(201).json({
+      message: `Temperature: ${Temperature}°, Humidity: ${Humidity}%, Sensor number: ${Sensornr}, Timestamp: ${datetime} added successfully.`,
+      insertId: result.insertId
+    });
+}catch(error) {
+    res.status(500).json({ error: error.message });
+}
+
+});
 
 router.use((req, res) => {
   res.status(404).json({ message: 'Route nicht gefunden - route' });
